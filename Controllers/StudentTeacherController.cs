85dbc89663d40088fa8faa24c4ef725853576797
Suchi Siwach <46921160+Suchitech02@users.noreply.github.com>
2020-10-13 17:32:44 +0200

    controller using ExpandoObject

--- Controllers/StudentTeacherController.cs
+++ Controllers/StudentTeacherController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Dynamic;
+using MVCModelView.Models;
+
 
 namespace MVCModelView.Controllers
 {
     public class StudentTeacherController : Controller
     {
         // GET: StudentTeacher
-        public ActionResult Index()
+        public ActionResult Details()
         {
-            return View();
+            dynamic container = new ExpandoObject();
+            StudentDAL obj = new StudentDAL();
+            container.st = obj.GetStudents();
+
+            TeacherDAL obj1 = new TeacherDAL();
+            container.tea = obj1.GetTeachers();
+            return View(container);
         }
     }
 }
