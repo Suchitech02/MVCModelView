08f780f4b5578410ffe5359adc525fdefb31b289
Suchi Siwach <46921160+Suchitech02@users.noreply.github.com>
2020-10-13 17:22:29 +0200

    TeachersDAL

---
+++ Controllers/StudentTeacherController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCModelView.Controllers
+{
+    public class StudentTeacherController : Controller
+    {
+        // GET: StudentTeacher
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Models/TeacherDAL.cs
+++ Models/TeacherDAL.cs
@@ -6,5 +6,35 @@ namespace MVCModelView.Models
 {
     public class TeacherDAL
     {
+        public List<Teacher> GetTeachers()
+        {
+            List<Teacher> teachers = new List<Teacher>();
+            teachers.Add(new Teacher
+            {
+                TeacherId = 101,
+                Name = "Ramesh",
+                Code = "Ram101"
+            });
+            teachers.Add(new Teacher
+            {
+                TeacherId = 102,
+                Name = "Ali",
+                Code = "Ali102"
+            });
+            teachers.Add(new Teacher
+            {
+                TeacherId = 103,
+                Name = "Chris",
+                Code = "Chris103"
+            });
+            teachers.Add(new Teacher
+            {
+                TeacherId = 104,
+                Name = "Bal",
+                Code = "Bal104"
+            });
+            return teachers;
+
+        }
     }
 }
